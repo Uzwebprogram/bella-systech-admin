50d427ee47d30008f26d591e9028c2ded221ef64
SarvinozSupersite <Supersite.office@gmail.com>
2023-08-31 13:38:59 +0500

    go push

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
-echo "Switching to branch main"
-git checkout main
+echo "Switching to branch saidbek"
+git checkout saidbek
 
 echo "Building app..."
 npm run build
 
 echo "Deploying files to server..."
-scp -r build/* root@62.217.179.24:/var/www/mardtravel-admin/
+scp -r build/* root@138.197.16.184:/var/www/bella-admin/
 
 echo "Done!"
